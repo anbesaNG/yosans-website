10e72cf8b810834ffcf4f60e8ffa194cc369eff7
yosanw <98075559+yosanw@users.noreply.github.com>
2022-03-23 12:26:46 -0500

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,10 +6,8 @@ function MainPage() {
     return (
         <div className='page'>
             <h1>Objective</h1>
-            <p>Fere Project  aims to expand higher education acceptance and retention in the Eritrean refugee youth population located in Sudan. This website serves as a centralized database of international and domestic opportunities that students might not be aware of.</p>
-            <p>For students: Under the resources tab, we have posted information on how to craft your application profile so that you are one of the competitive candidates wherever you choose to apply. We have also posted a few tips on how to prepare for interviews. In addition, we will constantly research and post scholarship opportunities, undergraduate and masters programs that you qualify for.</p>
-            <p>For individuals living abroad: Financial burden is one of the main reasons students are not able to continue their studies past high school education. Choosing to sponsor one student would greatly help in tackling this issue. Go to “Support Fere Project” for more information. If there are other scholarships or opportunities we have not posted, please send us an email to inform us. </p>
-            <p>This is a small-scale project working on a community level. We hope this instills hope and offers stability in the Eritrean community in Sudan.</p>
+            <p>Fere (Seed) Project aims to support young Eritrean refugees in Sudan to reach their full potential by connecting them to educational opportunities. The adequate development of a country with a strong institution and just policies depends on current and future young leaders. Supporting young high-achieving students financially or information-wise is a great first step in ensuring African youth will critically challenge issues of equality and social injustice in their community. 
+
         </div>
     );
 }
